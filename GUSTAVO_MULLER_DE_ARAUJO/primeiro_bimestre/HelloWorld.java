107c228d4f3e21ee264121b6a8d585cddcb5c700
gmaraujo1 <gmaraujo1@minha.fag.edu.br>
2024-03-08 20:39:58 -0300

    Lista1

--- GUSTAVO_MULLER_DE_ARAUJO/primeiro_bimestre/HelloWorld.java
+++ GUSTAVO_MULLER_DE_ARAUJO/primeiro_bimestre/HelloWorld.java
@@ -1,5 +1,15 @@
 package primeiro_bimestre;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe sua idade: ");
+    int idade = scanner.nextInt();
+    System.out.println("Minha idade é: " + idade);        
+    }
 }
+
+ 
