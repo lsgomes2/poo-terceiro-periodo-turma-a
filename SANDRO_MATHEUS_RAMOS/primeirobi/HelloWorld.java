0086aab50c1a0b118f5c03985d0e1cb91ef9a639
llshiratsu <llshiratsu@minha.fag.edu.br>
2024-03-10 15:21:28 -0300

    Commit donagabriela Leonardo Shiratsu

--- SANDRO_MATHEUS_RAMOS/primeirobi/HelloWorld.java
+++ SANDRO_MATHEUS_RAMOS/primeirobi/HelloWorld.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Informe sua idade: ");
@@ -11,6 +12,8 @@ public class HelloWorld {
 
         System.out.println("Minha idade é: " + idade);
         System.out.println(idade < 18 ? "É menor" : "Não é menor");
+
+        scanner.close();
     }
 
 }
