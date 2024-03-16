31205d31f468d8a949b437405a6d5126727eaacc
mateusduarte05 <mmduarte@minha.fag.edu.br>
2024-03-16 15:56:32 -0300

    modified:   MATEUS_MASSUCATTO_DUARTE/primeirobimestre/Calculadora.java lista 2

--- MATEUS_MASSUCATTO_DUARTE/primeirobimestre/Calculadora.java
+++ MATEUS_MASSUCATTO_DUARTE/primeirobimestre/Calculadora.java
@@ -1,9 +1,13 @@
 package primeirobimestre;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Calculadora 
 {
+    private static ArrayList<Integer> listaQuantidade = new ArrayList<>();
+    private static ArrayList<Double> listaPreco = new ArrayList<>();
+    private static ArrayList<Integer> listaDesconto = new ArrayList<>();
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +29,13 @@ public class Calculadora
                 case 3:
                     System.out.println("Obrigado por usar a calculadora!");
                     break;
+                case 4:
+                    System.out.println("Histórico de vendas: \n");
+                    for (int i = 0; i < listaQuantidade.size(); i++)
+                    {
+                        System.out.println("\n Quantidade: " + listaQuantidade.get(i) + "\n Preço: " + listaPreco.get(i) + "\n Desconto: " + listaDesconto.get(i) + "%");
+                    } 
+                    break;   
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
@@ -36,6 +47,7 @@ public class Calculadora
         System.out.print("1 - Calcular preço total \n");
         System.out.print("2 - Calcular troco \n");
         System.out.print("3 - Sair \n");
+        System.out.println("4 - Ver histórico de vendas \n");
         System.out.print("Digite a opção desejada: ");
     }
 
@@ -43,13 +55,18 @@ public class Calculadora
     {
         System.out.print("Digite a quantidade do item: ");
         int quantidade = scanner.nextInt();
-
         System.out.print("Digite o preço do item: ");
         int precoUnitario = scanner.nextInt();
-
-        int precoTotal = quantidade * precoUnitario;
-
-        System.out.printf("O preço total é: %d\n", precoTotal);
+        double precoTotal = quantidade * precoUnitario;
+        System.out.printf("O preço total é: %.2f\n", precoTotal);
+        if (quantidade >= 10) {
+            precoTotal = precoTotal - (precoTotal * 0.05);
+            System.out.printf("o preço total com desconto é: %.2f\n", precoTotal);
+        }
+        
+        listaQuantidade.add(quantidade);
+        listaPreco.add(precoTotal);
+        listaDesconto.add(quantidade >=10 ? 5 : 0);
     }
 
     private static void calcularTroco(Scanner scanner) 
