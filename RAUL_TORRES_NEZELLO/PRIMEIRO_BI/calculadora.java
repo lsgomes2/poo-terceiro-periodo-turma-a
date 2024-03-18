88b8f283dac644a16cc0ae4579fc4b1450f53b88
raultorresn <raultorresn@hotmail.com>
2024-03-18 19:37:22 -0300

    Lista 2 - melhorias calculadora

--- RAUL_TORRES_NEZELLO/PRIMEIRO_BI/calculadora.java
+++ RAUL_TORRES_NEZELLO/PRIMEIRO_BI/calculadora.java
@@ -5,40 +5,61 @@ import java.util.Scanner;
 public class calculadora {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Quantidade da planta:");
-            int qnt = scanner.nextInt();
-
-            System.out.println("Valor da planta:");
-            int val = scanner.nextInt();
-
-            int tot = val * qnt;
-
-            System.out.println("Valor recebido:");
-            int receba = scanner.nextInt();
-
-            int verifi = 0;
-
-            while(verifi == 0){
-            System.out.println("Menu\n [1]Preço total\n [2]Troco\n [3]Sair");
-            int menu = scanner.nextInt();
-            if(menu==1){
-                System.out.println("Preço total: "+tot);
-            }
-            if(menu==2){
-                if(tot<receba){
-                System.out.println("Troco a ser dado: "+(receba - tot));
+            int maisproduto;
+            int[] vetor = new int[100];
+            int contador=0;
+            do {
+                System.out.println("Quantidade da planta:");
+                int qnt = scanner.nextInt();
+    
+                System.out.println("Valor da planta:");
+                int val = scanner.nextInt();
+                
+                int tot = val * qnt;
+                if(qnt>10){
+                    tot = (int) (tot - (0.1 * (double) tot));
                 }
-                if(receba<tot){
-                    System.out.println("Valor faltando:"+(tot - receba));
+                vetor[contador] = tot;
+
+                System.out.println("Valor recebido:");
+                int receba = scanner.nextInt();
+    
+                int verifi = 0;
+                contador++;
+                while(verifi == 0){
+                System.out.println("Menu\n [1]Preço total\n [2]Troco\n [3]Registro de vendas\n [4]Sair");
+                int menu = scanner.nextInt();
+                switch (menu) {
+                    case 1:
+                    System.out.println("Preço total: "+tot);
+                        break;
+                    case 2:
+                    if(tot<receba){
+                        System.out.println("Troco a ser dado: "+(receba - tot));
+                        }
+                        if(receba<tot){
+                            System.out.println("Valor faltando:"+(tot - receba));
+                        }
+                        break;
+                    case 3:
+                    for(int i=0;i<contador;i++){
+                       int n=i+ 1; 
+                        System.out.println("Compra "+n+": "+vetor[i]);
+                
+                    }
+    
+                        break;
+                    case 4:
+                    verifi = 1;
+                        break;
+                    default:
+                    System.out.println("Numero invalido");
+                        break;
                 }
-            }
-            if(menu==3){
-                verifi = 1;
-            }
-            if(menu<1||menu>3){
-                System.out.println("Numero invalido");
-            }
-     }
+         }
+         System.out.println("Deseja adicionar mais um produto?\n[1]Sim\n[2]Nao");
+         maisproduto = scanner.nextInt();
+            } while (maisproduto==1);
         }
     }
 }
