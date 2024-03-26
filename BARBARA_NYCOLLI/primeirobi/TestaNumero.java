373280012b51e4ada88d6d62931a401a69de084f
barbaranycolli <nycollibarbara@gmail.com>
2024-03-25 21:08:14 -0300

    entregas exercicio 3

--- BARBARA_NYCOLLI/primeirobi/TestaNumero.java
+++ BARBARA_NYCOLLI/primeirobi/TestaNumero.java
@@ -1,5 +1,4 @@
 package primeirobi;
-
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,6 +6,7 @@ public class TestaNumero {
     public static void main(String[] args) {
         Numero n = new Numero();
         HashMap<String, Double> historico = new HashMap<>();
+        HashMap<String, Double> vendasPorDia = new HashMap<>(); 
 
         boolean continuar = true;
         Scanner scan = new Scanner(System.in);
@@ -17,8 +17,12 @@ public class TestaNumero {
             System.out.println("Menu Console:\n" +
                     "[1] - Calcular Preço Total\n" +
                     "[2] - Calcular Troco\n" +
-                    "[3] - Consultar Histórico\n" +
-                    "[4] - Sair");
+                    "[3] - Calcular Valor a Pagar\n" +
+                    "[4] - Calcular Quantidade de Dias em um Número de Meses\n" +
+                    "[5] - Salvar Venda Diária\n" +
+                    "[6] - Consultar Vendas por Dia\n" +
+                    "[7] - Consultar Histórico\n" +
+                    "[8] - Sair");
             System.out.print("Escolha uma opção: ");
             escolha = scan.nextInt();
 
@@ -32,10 +36,26 @@ public class TestaNumero {
                     break;
 
                 case 3:
-                    consultarHistorico(historico);
+                    calcularValorAPagar(n, scan, historico);
                     break;
 
                 case 4:
+                    calcularDiasEmMeses(scan, historico);
+                    break;
+
+                case 5:
+                    salvarVendaDiaria(scan, vendasPorDia);
+                    break;
+
+                case 6:
+                    consultarVendasPorDia(scan, vendasPorDia);
+                    break;
+
+                case 7:
+                    consultarHistorico(historico);
+                    break;
+
+                case 8:
                     continuar = false;
                     System.out.println("Programa encerrado");
                     break;
@@ -49,44 +69,56 @@ public class TestaNumero {
     }
 
     static void calcularPrecoTotal(Numero n, Scanner scan, HashMap<String, Double> historico) {
-        System.out.println("Digite a quantidade da planta: ");
-        int quantidadePlanta = scan.nextInt();
-        System.out.println("Digite o preço da planta: ");
-        double precoPlanta = scan.nextDouble();
-
-        double precoTotal;
-        if (quantidadePlanta > 10) {
-            double desconto = precoPlanta * 0.1;
-            precoTotal = n.multiplicacao(precoPlanta, quantidadePlanta) - desconto;
-            System.out.println("Desconto de 10% aplicado.");
-        } else {
-            precoTotal = n.multiplicacao(precoPlanta, quantidadePlanta);
-        }
-
-        System.out.println("Preço total: " + precoTotal);
-
-        historico.put("Preço Total", precoTotal);
+     
     }
 
     static void calcularTroco(Numero n, Scanner scan, HashMap<String, Double> historico) {
-        System.out.println("Digite o valor pago: ");
-        double valorPago = scan.nextDouble();
-        System.out.println("Digite o preço do produto: ");
-        double precoProduto = scan.nextDouble();
+  
+    }
 
-        if (valorPago >= precoProduto) {
-            double troco = n.subtracao(valorPago, precoProduto);
-            System.out.println("Valor do troco: " + troco);
-            historico.put("Venda Realizada", precoProduto);
+    static void calcularValorAPagar(Numero n, Scanner scan, HashMap<String, Double> historico) {
+      
+    }
+
+    static void calcularDiasEmMeses(Scanner scan, HashMap<String, Double> historico) {
+        
+    }
+
+    static void salvarVendaDiaria(Scanner scan, HashMap<String, Double> vendasPorDia) {
+        System.out.println("Digite o mês da venda: ");
+        int mes = scan.nextInt();
+        System.out.println("Digite o dia da venda: ");
+        int dia = scan.nextInt();
+        System.out.println("Digite o valor da venda: ");
+        double valorVenda = scan.nextDouble();
+
+        String chave = mes + "/" + dia;
+        if (vendasPorDia.containsKey(chave)) {
+            double vendasTotais = vendasPorDia.get(chave) + valorVenda;
+            vendasPorDia.put(chave, vendasTotais);
         } else {
-            System.out.println("Valor pago é insuficiente para o preço do produto.");
+            vendasPorDia.put(chave, valorVenda);
+        }
+
+        System.out.println("Venda diária salva com sucesso!");
+    }
+
+    static void consultarVendasPorDia(Scanner scan, HashMap<String, Double> vendasPorDia) {
+        System.out.println("Digite o mês da venda: ");
+        int mes = scan.nextInt();
+        System.out.println("Digite o dia da venda: ");
+        int dia = scan.nextInt();
+
+        String chave = mes + "/" + dia;
+        if (vendasPorDia.containsKey(chave)) {
+            double vendasTotais = vendasPorDia.get(chave);
+            System.out.println("Vendas totais para o dia " + dia + " do mês " + mes + ": " + vendasTotais);
+        } else {
+            System.out.println("Não há vendas registradas para o dia " + dia + " do mês " + mes);
         }
     }
 
     static void consultarHistorico(HashMap<String, Double> historico) {
-        System.out.println("Histórico de Transações:");
-        for (String chave : historico.keySet()) {
-            System.out.println(chave + ": " + historico.get(chave));
-        }
+  
     }
 }
