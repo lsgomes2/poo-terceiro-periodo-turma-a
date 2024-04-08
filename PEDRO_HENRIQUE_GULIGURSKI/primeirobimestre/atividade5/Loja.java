2ee9e14af1cd3c659036ee81ca007d627162c2b3
pedroguligurski <pedro.sguligurski@gmail.com>
2024-04-08 20:33:40 -0300

    Lista 5 - Classes

--- PEDRO_HENRIQUE_GULIGURSKI/primeirobimestre/atividade5/Loja.java
+++ PEDRO_HENRIQUE_GULIGURSKI/primeirobimestre/atividade5/Loja.java
@@ -2,11 +2,55 @@ package primeirobimestre.atividade5;
 
 public class Loja {
     
-    String nomeFantasia;
-    String razaoSocial;
-    String cnpj;
-    String[] endereco = new String[3];
+    private String nomeFantasia;
+    @SuppressWarnings("unused")
+    private String razaoSocial;
+    private String cnpj;
+    private String[] endereco = new String[3]; //0 - cidade, 1 - bairro, 2 - rua
     Vendedor[] vendedores = new Vendedor[30];
     Cliente[] clientes = new Cliente[30];
+
+    public Loja(String nomeFantasia, String razaoSocial, String cnpj,String[] endereco, Vendedor[] vendedores, Cliente[] clientes) {
+        this.nomeFantasia = nomeFantasia;
+        this.razaoSocial = razaoSocial;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.vendedores = vendedores;
+        this.clientes = clientes;
+        
+    }
+
+    public void apresentarse() {
+        String msg = "Bem-vindo a "
+                .concat(nomeFantasia + "\n")
+                .concat("CNPJ: ")
+                .concat(cnpj +".\n")
+                .concat("Me localizo em ")
+                .concat(endereco[0]+", "+endereco[1]+", "+endereco[2])
+                .concat(".");
+        System.out.println(msg);
+    }
+
+    public int contarClientes() {
+        int contador = 0;
+        for (Cliente cliente : clientes) {
+            if (cliente != null) {
+                contador++;
+            }
+        }
+        System.out.println("Número de clientes: " + contador);
+        return contador;
+    }
+
+    public int contarVendedores() {
+        int contador = 0;
+        for (Vendedor vendedor : vendedores) {
+            if (vendedor != null) {
+                contador++; 
+            }
+        }
+        System.out.println("Número de clientes: " + contador);
+        return contador;
+    }
 
 }
