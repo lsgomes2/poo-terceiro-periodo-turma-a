b30f1e0fe9a5808a03a51790eb18b45279926b5a
llshiratsu <llshiratsu@minha.fag.edu.br>
2024-04-08 16:06:12 -0300

    Commit Loja lista 5

---
+++ LEONARDO_LIOGI_SHIRATSU/primbi/cliente.java
@@ -0,0 +1,14 @@
+package primbi;
+
+public class cliente {
+
+    String nome;
+    String cidade;
+    String rua;
+    String bairro;
+    int idade;
+    
+    void apresentarseCliente(){
+        System.out.println("\nOlá meu nome é " + nome + " sou cliente da TrabucoStore e tenho " + idade + " anos");
+    }
+}
---
+++ LEONARDO_LIOGI_SHIRATSU/primbi/loja.java
@@ -0,0 +1,30 @@
+package primbi;
+
+public class loja {
+    //Atributos, nome fantasia, razão social, cpnj, cidade, bairro, rua, array vendedores, array de clientes.
+
+    String nome;
+    String razaosocial;
+    String cnpj;
+    String cidade;
+    String bairro;
+    String rua;
+    double[] quantVendedores = {21,30,22};
+    double quantClientes[] = {2,3,3,3,3};
+
+
+    public void apresentarSe() {
+        System.out.println("\nNome Fantasia: " + nome + "\nCNPJ: " + cnpj +
+                "\nEndereço: " + cidade + ", " + bairro + ", " + rua);
+    }
+
+    public void contarClientes() {
+        System.out.println("Quantidade de clientes: " + quantClientes.length);
+    }
+
+    void contarVendedores(){
+
+        System.out.println("Quantidade de vendedores: " + quantVendedores.length);
+
+    }
+}
