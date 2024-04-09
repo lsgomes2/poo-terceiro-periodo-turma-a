32d7c56f16f1d54c3fcc7b308b26b622395cca95
JewelDrop <eduardojsczerniak@yahoo.com>
2024-04-08 23:56:49 -0300

    MyyPlant

---
+++ EDUARDO_JOSE_SKURA_CZERNIAK/primeirobi/listas/myyplant/Cliente.java
@@ -0,0 +1,15 @@
+package primeirobi.listas.myyplant;
+
+public class Cliente {
+    
+    String nome;
+    Integer idade;
+    String cidade;
+    String bairro;
+    String rua;
+
+    public void apresentarCliente() {
+        System.out.println(" Olá me chamo " + nome + " e tenho " + idade + " anos! ");
+    }
+
+}
