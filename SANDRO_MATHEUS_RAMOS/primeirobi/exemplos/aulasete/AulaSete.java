91c10c92f442413ce85f27e04ab9ee9a956e33f1
Sandrolaxx <sandro--ramos@hotmail.com>
2024-04-08 21:10:13 -0300

    add exemplos aula

--- SANDRO_MATHEUS_RAMOS/primeirobi/exemplos/aulasete/AulaSete.java
+++ SANDRO_MATHEUS_RAMOS/primeirobi/exemplos/aulasete/AulaSete.java
@@ -1,12 +1,14 @@
 package primeirobi.exemplos.aulasete;
 
+import java.util.Scanner;
+
 public class AulaSete {
     
     public static void main(String[] args) {
-        
+
         Ninja ninjaUm = new Ninja();
         Ninja ninjaDois = new Ninja();
-
+        
         ninjaUm.nome = "Naruto";
         ninjaUm.aldeia = "Folha";
         ninjaUm.cla = "Uzumaki";
@@ -27,15 +29,27 @@ public class AulaSete {
         // ninjaDois.apresentarse();
         // ninjaDois.realizarJutso();
 
-        Andorinha andorinha = new Andorinha();
-        AndorinhaAzul azul = new AndorinhaAzul();
+        // Andorinha andorinha = new Andorinha();
+        // AndorinhaAzul azul = new AndorinhaAzul();
 
-        andorinha.nome = "Cleber";
+        // andorinha.nome = "Cleber";
 
-        andorinha.bicar();
-        andorinha.voar();
+        // andorinha.bicar();
+        // andorinha.voar();
 
-        azul.bicar();
+        // azul.bicar();
+
+        Aluno aluno = new Aluno();
+        Professor profe = new Professor();
+        Laboratorista lab = new Laboratorista();
+
+        verificar(aluno);
+        verificar(profe);
+        verificar(lab);
+    }
+    
+    public static void verificar(PessoaAbstrata pessoa) {
+        pessoa.apresentarse();
     }
 
 }
