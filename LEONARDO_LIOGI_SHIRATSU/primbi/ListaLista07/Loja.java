bb5008ccd00b93fe7a62896729fbca22663286b0
llshiratsu <llshiratsu@minha.fag.edu.br>
2024-04-22 07:39:04 -0300

    Commit 07

---
+++ LEONARDO_LIOGI_SHIRATSU/primbi/ListaLista07/Loja.java
@@ -0,0 +1,15 @@
+package primbi.ListaLista07;
+
+class Loja extends Funcionario {
+    public Loja(String nome, int idade, String cidade, String bairro, String rua) {
+        super.setNome(nome);
+        super.setIdade(idade);
+        super.setCidade(cidade);
+        super.setBairro(bairro);
+        super.setRua(rua);
+
+
+    }
+    
+
+}
